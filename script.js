c4509525539d3ce13d52a530f24391a10c3d52cd
Rafal Krismann <rkrismann33@gmail.com>
2021-02-08 19:58:48 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://sleepy-scrubland-56128.herokuapp.com/v1/task';
+  var apiRoot = 'https://sleepy-scrubland-56128.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
